7afc59149b0d8e4ca5980f1a7eadb42b928b7502
Leila Touati <leilat014@gmail.com>
2025-11-18 16:50:21 -0800

    loaded more data into Mongo

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -23,17 +23,23 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
 ));
 var import_express = __toESM(require("express"));
 var import_mongo = require("./services/mongo");
+var import_traveler_svc = __toESM(require("./services/traveler-svc"));
 (0, import_mongo.connect)("miniature-journey");
 const app = (0, import_express.default)();
 const port = process.env.PORT || 3e3;
 const staticDir = process.env.STATIC || "public";
 app.use(import_express.default.static(staticDir));
-app.get(
-  "/hello",
-  (req, res) => {
-    res.send("Hello, World");
-  }
-);
+app.get("/travelers/:userid", (req, res) => {
+  const { userid } = req.params;
+  import_traveler_svc.default.get(userid).then((data) => {
+    if (data)
+      res.set("Content-Type", "application/json").send(JSON.stringify(data));
+    else res.status(404).send();
+  });
+});
+app.get("/hello", (req, res) => {
+  res.send("Hello, World");
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { connect } from "./services/mongo";
+import Travelers from "./services/traveler-svc";
 
 connect("miniature-journey");
 
@@ -9,9 +10,18 @@ const staticDir = process.env.STATIC || "public";
 
 app.use(express.static(staticDir));
 
+app.get("/travelers/:userid", (req: Request, res: Response) => {
+  const { userid } = req.params;
+
+  Travelers.get(userid).then((data: any) => {
+    if (data)
+      res.set("Content-Type", "application/json").send(JSON.stringify(data));
+    else res.status(404).send();
+  });
+});
 app.get("/hello", (req: Request, res: Response) => {
-    res.send("Hello, World");}
-);
+  res.send("Hello, World");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
